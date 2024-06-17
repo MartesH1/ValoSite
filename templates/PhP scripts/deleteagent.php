2cf8bbc6638df8c61b5f2d8f86236c39bac16298
MartesH1 <99804014+MartesH1@users.noreply.github.com>
2024-06-17 18:14:03 +0500

    Update deleteagent.php

--- templates/PhP scripts/deleteagent.php
+++ templates/PhP scripts/deleteagent.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_COOKIE['agent_id'];
-$mysql = new mysqli('localhost', 'root', '', 'valo_bd');
+$mysql = new mysqli('localhost', 'root', '1111', 'valo_bd');
 $mysql -> query("DELETE FROM `agents`
 WHERE `id` = '$id'");
 $mysql -> close();
