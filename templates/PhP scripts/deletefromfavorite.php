0706c6f26d2cfd24e5f0396d3ccd944d93a35612
MartesH1 <99804014+MartesH1@users.noreply.github.com>
2024-06-17 18:14:17 +0500

    Update deletefromfavorite.php

--- templates/PhP scripts/deletefromfavorite.php
+++ templates/PhP scripts/deletefromfavorite.php
@@ -2,7 +2,7 @@
 
 $user_id = $_GET['user_id'];
 $agent_id = $_GET['agent_id'];
-    $mysql = new mysqli('localhost', 'root', '', 'valo_bd');
+    $mysql = new mysqli('localhost', 'root', '1111', 'valo_bd');
     $mysql -> query("DELETE FROM `favorite`
     WHERE `user_id` = '$user_id' AND `agent_id` = '$agent_id'");
     $mysql -> close();
