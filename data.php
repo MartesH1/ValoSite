5352ae1f0c5c9b247ce20e67b3aeaf03cc009c56
MartesH1 <99804014+MartesH1@users.noreply.github.com>
2024-06-17 18:19:27 +0500

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
 
-$mysql = new mysqli('localhost', 'root', '', 'valo_bd');
+$mysql = new mysqli('localhost', 'root', '1111', 'valo_bd');
 $characters_list = $mysql -> query("SELECT * FROM `agents` 
 WHERE 1");
